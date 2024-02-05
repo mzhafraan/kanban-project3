8fc7759c5aa81ee1a607689e561c8f85ce397b11
NinjaPran <mzhafraan@gmail.com>
2024-02-05 15:07:33 +0700

    Apply Common Layout

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\TaskController;
 
 use Illuminate\Http\Request;
 
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-
     return view('home');
-
 })->name('home'); // name ditambahkan
 
 
